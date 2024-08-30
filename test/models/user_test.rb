947bbcf9d94b3d04ba8a6398b4349cd7cb40a4ef
shimiharu1012 <shimizuharuki314@gmail.com>
2024-08-30 15:34:26 +0000

    6章終了 user_test,model/userの記述

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: 'Example User', email: "user@example.com")
+    @user = User.new(name: 'Example User', email: 'user@example.com',
+                     password: 'password', password_confirmation: 'password')
   end
 
   test 'should be valid' do
@@ -38,7 +39,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test 'email validation should reject invalid addresses' do
-    invalid_addresses = ['user@example,com', 'user@example..com', 'user_at_foo.org', 'user.name@example.',
+    invalid_addresses = ['user@example,com', 'user_at_foo.org', 'user.name@example.',
                          'foo@bar_baz.com', 'foo@bar+baz.com']
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
@@ -51,5 +52,22 @@ class UserTest < ActiveSupport::TestCase
     dupulicate_user_email = @user.email.upcase
     @user.save
     assert_not dupulicate_user.valid?
+  end
+
+  test 'emailアドレスは小文字として保存されなくてはならない' do
+    mixed_case_email = 'Foo@ExAMPle.CoM'
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test 'パスワードは0文字以上でなくてはならない' do
+    @user.password = @user.password_confirmation = ' ' * 8
+    assert_not @user.valid?, "#{@user.name}"
+  end
+
+  test 'パスワードは最小長が決まっている' do
+    @user.password = @user.password_confirmation = 'a' * 7
+    assert_not @user.valid?, "#{@user.password}"
   end
 end
