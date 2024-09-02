42bcbc5250beb5dc6b971e08c4530c5d98de42aa
shimiharu1012 <shimizu1012@ezweb.ne.jp>
2024-09-02 17:13:41 +0900

    [hotfix] rander-build.shの記述を元に戻す

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,6 +5,6 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
+# DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
 
-# bundle exec rails db:migrate
+bundle exec rails db:migrate
