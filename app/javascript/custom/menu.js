dfe44c1876f9a1a770901ea915ef670f382d436b
shimiharu1012 <shimizu1012@ezweb.ne.jp>
2024-09-06 07:56:51 +0900

    [add] 8.2.5 ハンバーガーメニューおよびfooterにレスポンシブデザインを追加

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -1,14 +1,19 @@
 // メニュー操作
 
+//　トグルリスナーを追加する
+
+function addToggleListener(selected_id,menu_id,toggle_class){
+    let selected_element=document.querySelector(`#${selected_id}`)
+    selected_element.addEventListener("click",function(e){
+        e.preventDefault();
+        let menu= document.querySelector(`#${menu_id}`);
+        menu.classList.toggle(toggle_class);
+    })
+}
+
 // トグルリスナーを追加して、クリックをリッスン
 
-document.addEventListener("turbo:load", function(){
-    let account = document.querySelector("#account");
-    if (account) {
-        account.addEventListener("click",function(e){
-            e.preventDefault();
-            let menu=document.querySelector("#dropdown-menu");
-            menu.classList.toggle("active");
-        })
-    }
+document.addEventListener("turbo:load",function(){
+    addToggleListener("hamburger","navbar-menu","collapse");
+    addToggleListener("account","dropdown-menu","active");
 })
